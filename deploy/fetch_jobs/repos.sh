07a67edd9bb142dc5942278d12d2264c7ef4a571
syuhas <syuhas22@gmail.com>
2025-02-18 18:27:10 -0500

    correct branches list file

--- deploy/fetch_jobs/repos.sh
+++ deploy/fetch_jobs/repos.sh
@@ -50,5 +50,5 @@ echo "}" >> "$BRANCHES_LIST_FILE"
 echo "Branches saved to $BRANCHES_LIST_FILE"
 
 mkdir -p "/home/jenkins_home/github"
-cp -p "$WORKSPACE/$BRANCHES_LIST_FILE" "$JENKINS_HOME/github/github_branches.json"
+cp -p "$BRANCHES_LIST_FILE" "$JENKINS_HOME/github/github_branches.json"
 
