d74308062395b7104f74e68c892d36f4962f8858
syuhas <syuhas22@gmail.com>
2025-02-19 16:00:53 -0500

    remove sb

--- dsl/seed_job_ecs_deployer.groovy
+++ dsl/seed_job_ecs_deployer.groovy
@@ -3,55 +3,40 @@ pipelineJob('test_ecs-deployer') {
 
     // General configurations
     parameters {
-        choiceParameter {
-            name('Repository')
-            description('Select a repository to deploy')
-            choiceType('PT_SINGLE_SELECT') // Single selection dropdown
-
-            script {
-                groovyScript {
-                    script("""
-                        import groovy.json.JsonSlurper
-                        def jsonFile = new File(System.getenv('JENKINS_HOME') + "/github/github_branches.json")
-                        if (!jsonFile.exists()) {
-                            return ["No Repositories Found"]
-                        }
-                        def jsonContent = new JsonSlurper().parseText(jsonFile.text)
-                        return jsonContent.keySet().toList()
-                    """)
-                    sandbox(false)
-                }
-                fallbackScript {
-                    script("return ['No Repositories Available']")
-                    sandbox(true)
-                }
+        activeChoiceParam('Repository') {
+            description('Select a repository.')
+            choiceType('SINGLE_SELECT')
+            groovyScript {
+                script('''
+                    import groovy.json.JsonSlurper
+                    def jenkinsHome = System.getenv("JENKINS_HOME")
+                    def jsonFile = new File("${jenkinsHome}/github/github_branches.json")
+                    if (!jsonFile.exists()) {
+                        return ["No Repositories Found"]
+                    }
+                    def jsonContent = new JsonSlurper().parseText(jsonFile.text)
+                    return jsonContent.keySet().toList()
+                ''')
             }
         }
 
-        choiceParameter {
-            name('Branch')
-            description('Select a branch for the selected repository')
-            choiceType('PT_RADIO') // Radio button selection
-            referencedParameters(['Repository'])
+        activeChoiceReactiveParam('Branch') {
+            description('Select a branch based on the repository.')
+            choiceType('RADIO')
+            referencedParameter('Repository')
+            groovyScript {
+                script('''
+                    import groovy.json.JsonSlurper
+                    def jenkinsHome = System.getenv("JENKINS_HOME")
+                    def jsonFile = new File("${jenkinsHome}/github/github_branches.json")
+                    if (!jsonFile.exists()) {
+                        return ["No Branches Found"]
+                    }
+                    def jsonContent = new JsonSlurper().parseText(jsonFile.text)
+                    def branches = jsonContent[Repository] ?: ["No Branches Found"]
+                    return branches
 
-            script {
-                groovyScript {
-                    script("""
-                        import groovy.json.JsonSlurper
-                        def jsonFile = new File(System.getenv('JENKINS_HOME') + "/github/github_branches.json")
-                        if (!jsonFile.exists()) {
-                            return ["No Branches Found"]
-                        }
-                        def jsonContent = new JsonSlurper().parseText(jsonFile.text)
-                        def branches = jsonContent[Repository] ?: ["No Branches Found"]
-                        return branches
-                    """)
-                    sandbox(false)
-                }
-                fallbackScript {
-                    script("return ['No Branches Available']")
-                    sandbox(true)
-                }
+                ''')
             }
         }
 
@@ -59,33 +44,24 @@ pipelineJob('test_ecs-deployer') {
         choiceParam('Account', ['551796573889', '061039789243'], 'Select an AWS account.')
         stringParam('Subdomain', 'test', 'Subdomain to deploy (eg. php = php.example.net or php.dev.example.net).')
 
-        choiceParameter {
-            name('Subdomains Currently In Use')
-            description('List of subdomains currently in use')
-            choiceType('PT_MULTI_SELECT') // Allows multiple selections
-
-            script {
-                groovyScript {
-                    script("""
-                        import groovy.json.JsonSlurper
-                        def filePath = System.getenv('JENKINS_HOME') + "/workspace/fetch_subdomains/subdomain.json"
-                        def subdomainsList = []
-                        if (new File(filePath).exists()) {
-                            def jsonText = new File(filePath).text
-                            subdomainsList = new JsonSlurper().parseText(jsonText)
-                        }
-                        if (subdomainsList.isEmpty()) {
-                            return ["No subdomains currently in use."]
-                        } else {
-                            return subdomainsList
-                        }
-                    """)
-                    sandbox(false)
-                }
-                fallbackScript {
-                    script("return ['No Subdomains Available']")
-                    sandbox(true)
-                }
+        activeChoiceParam('Subdomains Currently In Use') {
+            description('Displays currently used subdomains.')
+            choiceType('MULTI_SELECT')
+            groovyScript {
+                script('''
+                    import groovy.json.JsonSlurper
+                    def filePath = "/home/jenkins_home/workspace/fetch_subdomains/subdomains.json"
+                    def subdomainsList = []
+                    if (new File(filePath).exists()) {
+                        def jsonText = new File(filePath).text
+                        subdomainsList = new JsonSlurper().parseText(jsonText)
+                    }
+                    if (subdomainsList.isEmpty()) {
+                        return ["No subdomains currently in use."]
+                    } else {
+                        return subdomainsList
+                    }
+                ''')
             }
         }
     }
